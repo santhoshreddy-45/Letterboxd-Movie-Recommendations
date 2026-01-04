69a1eec37b1defd2c1646eb5ae61592f5e0f28ee
Victor Verma <victor.verma@hotmail.com>
2026-01-04 13:52:29 -0500

    updated footer

--- frontend/src/components/Layout/Footer.tsx
+++ frontend/src/components/Layout/Footer.tsx
@@ -47,7 +47,7 @@ const helpLinks = [
         text: "Feedback/Bugs",
         link: "https://docs.google.com/forms/d/e/1FAIpQLSfjRGNe8ORq0twg8hhzXiC3dNCqFErf3upZFuKtaafzrhgv0g/viewform?usp=header",
     },
-    { text: "Support the Project", link: "https://ko-fi.com/victorverma" },
+    { text: "Support this Site", link: "https://ko-fi.com/victorverma" },
 ];
 
 const Footer = () => {
