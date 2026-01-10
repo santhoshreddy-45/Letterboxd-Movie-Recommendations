00350a436c2db676c3996527f3e5e16f222629ff
Victor Verma <victor.verma@hotmail.com>
2026-01-09 23:08:22 -0500

    trimmed description

--- frontend/src/components/FilterDescription.tsx
+++ frontend/src/components/FilterDescription.tsx
@@ -42,7 +42,7 @@ const FilterDescription = () => {
                     dispatch({
                         type: "setDescription",
                         payload: {
-                            description: e.target.value,
+                            description: e.target.value.trim(),
                         },
                     })
                 }
